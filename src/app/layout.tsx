6da2aa6b9b4df5cf9d4364877783a7a0e3a02411
Tanmay Tripathi <tanmaytrip59@gmail.com>
2025-07-16 19:20:05 +0530

    chck

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Tanmay Tripathi - Portfolio",
+  title: "Tanmay Tripathi - About me",
   description:
     "Portfolio of Tanmay Tripathi - AI enthusiast. EEE student at VIT Vellore with experience in C++, embedded systems, and cloud computing.",
   keywords: [
@@ -19,17 +19,17 @@ export const metadata: Metadata = {
   authors: [{ name: "Tanmay Tripathi" }],
   creator: "Tanmay Tripathi",
   openGraph: {
-    title: "Tanmay Tripathi - Portfolio",
+    title: "Tanmay Tripathi - About me",
     description:
       "Portfolio of Tanmay Tripathi - AI enthusiast. EEE student at VIT Vellore with experience in C++, embedded systems, and cloud computing.",
     url: "https://tanmay-tripathi.vercel.app",
-    siteName: "Tanmay Tripathi Portfolio",
+    siteName: "Tanmay Tripathi - About me",
     locale: "en_US",
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    title: "Tanmay Tripathi - Portfolio",
+    title: "Tanmay Tripathi - About me",
     description:
       "Portfolio of Tanmay Tripathi - AI enthusiast. EEE student at VIT Vellore with experience in C++, embedded systems, and cloud computing.",
     creator: "@tanmaytrip59",
